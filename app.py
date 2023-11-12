b947a58ed4ac816d4740cc5da2abc255d3d45138
BAKUGAN17 <124163225+BAKUGAN17@users.noreply.github.com>
2023-11-12 17:11:17 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
 from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-db = SQLAlchemy(app)
-
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    intro = db.Column(db.String(300), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Article %r' % self.id
 
 @app.route('/')
 @app.route('/home')
@@ -22,17 +8,19 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/about')
+@app.route('/imt')
 def about():
-    return render_template("about.html")
+    return render_template("IMT.html")
 
-@app.route('/counters')
-def counters():
-    return render_template("counters.html")
+@app.route('/cnt_calorie1')
+def cnt1():
+    return render_template("cnt1.html")
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return 'User page: ' + name + '-' + str(id)
+@app.route('/cnt_calorie2')
+def cnt2():
+    return render_template("cnt2.html")
+
+
 
 
 
